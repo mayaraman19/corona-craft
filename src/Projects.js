b82c7725ef4f87bfa362fb623810ffff10f9dd42
Damini Kaushik <daminik@alphonse.lan>
2020-05-16 03:10:55 -0700

    added form for posts

--- src/Projects.js
+++ src/Projects.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
-function Projects(){
-    return(
+function Projects() {
+    return (
         <div>
             <h1>Some Home Crafting Projects!</h1>
+            <Link to="/CreatePost"> Write a post!</Link>
         </div>
     );
 }
