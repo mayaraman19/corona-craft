df59f250f2fb8f48a5b2bc0d92ed00c373269a91
Alex Ginella <ginellalex@gmail.com>
2020-05-19 19:33:39 -0700

    added some links under projects page

--- src/Projects.js
+++ src/Projects.js
@@ -6,6 +6,11 @@ function Projects() {
         <div>
             <h1>Some Home Crafting Projects!</h1>
             <Link to="/CreatePost"> Write a post!</Link>
+            <div className = "projects">
+                <Link to="/Baking">Baking</Link>
+                <Link to="/Masks"> Masks</Link>
+                <Link to="/Baking"> Gardening</Link>
+            </div>
         </div>
     );
 }
