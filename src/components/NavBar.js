3c7a84b708e2c7466a54f0aa5eac42574b498ced
Alex Ginella <ginellalex@gmail.com>
2020-05-23 18:13:40 -0700

    forgot to commit these files oops

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ class NavBar extends Component{
     render(){
     return (
         <ul class="NavBar">
-            <li><NavLink exact activeClassName="active" to="/"> Home</NavLink></li>
+            <li ><NavLink exact activeClassName="active" to="/"> Home</NavLink></li>
             <li><NavLink to="/Projects" activeClassName="active"> Projects</NavLink></li>
             <li><NavLink to="/About" activeClassName="active"> About</NavLink></li>
             <li><NavLink to="/Login" activeClassName="active"> Login</NavLink></li>
