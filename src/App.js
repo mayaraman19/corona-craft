f46279b4000884fb8f87bc19718f63a528f18f02
Michelle <michellegunawan9999@outlook.com>
2020-06-01 20:37:42 -0700

    signup functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './components/logo.svg';
 import './App.css';
 import Home from './components/Home'
+
 import Projects from './components/Projects'
 import {BrowerRouter, Route, Link} from 'react-router-dom';
 import NavBar from './components/NavBar'
@@ -10,29 +11,31 @@ import CreatePost from './components/CreatePost';
 import Baking from './components/Baking'
 import Masks from './components/Masks'
 import Gardening from './components/Gardening'
-import Login from './components/Login';
 
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <div>
-      <NavBar />
-    <div className="App">
-      <div className='content'>
-        <Route exact path="/" component={Home}/>
-        <Route path="/Login" component={Login}/>
-        <div>
-          <Route exact path="/Projects" component={Projects}/>
-          <div className='projects'>
-                <Route path="/Projects/CreatePost" component={CreatePost}/>
-                <Route exact path="/Projects/Baking" component={Baking}/>
-                <Route exact path="/Projects/Masks" component={Masks}/>
-                <Route exact path="/Projects/Gardening" component={Gardening}/>
+    <NavBar />
+      <div className="App">
+        <div className='content'>
+          <Route exact path="/" component={Home}/>
+          <Route path="/Login" component={Login}/>
+          <Route path="/Signup" component={Signup}/>
+          <div>
+            <Route exact path="/Projects" component={Projects}/>
+            <div className='projects'>
+                  <Route path="/Projects/CreatePost" component={CreatePost}/>
+                  <Route exact path="/Projects/Baking" component={Baking}/>
+                  <Route exact path="/Projects/Masks" component={Masks}/>
+                  <Route exact path="/Projects/Gardening" component={Gardening}/>
+            </div>
           </div>
+          <Route exact path="/About" component={About}/>
         </div>
-        <Route exact path="/About" component={About}/>
       </div>
-    </div>
     </div>
   );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,34 +1,114 @@
 import React from 'react';
 import {Component} from 'react';
 import {NavLink} from 'react-router-dom';
+import axios from 'axios';
 
 class Signup extends Component {
+    constructor() {
+      super();
+      this.state = {
+        email: '',
+        password: '',
+        confirmPassword: '',
+        handle: '',
+        loading: false,
+        errors: {}
+      };
+    }
+  
+    handleSubmit = (event) => {
+      event.preventDefault();
+      this.setState({
+        loading: true
+      });
+      const userData = {
+        email: this.state.email,
+        password: this.state.password,
+        confirmPassword: this.state.confirmPassword,
+        handle: this.state.handle
+      };
+      axios
+        .post('/signup', userData)
+        .then((res) => {
+          console.log(res.data);
+          localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
+          this.setState({
+            loading: false
+          });
+          this.props.history.push('/');
+        })
+        .catch((err) => {
+          this.setState({
+            errors: err.response.data,
+            loading: false
+          });
+        });
+    };
+    handleChange = (event) => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    };
     render() {
-        return( //style={styles.background} >
-            <div className="signup" > 
-                <h3>Welcome to CoronaCraft!</h3>
-                <form style={styles.form}>
-                    <p>
-                        <label className="textfield">Username</label>
-                        <input/>
-                    </p>
-                    <p>
-                        <label className="textfield">Password</label>
-                        <input type="password"/>
-                    </p>
-                    <p>
-                        <label className="textfield">Confirm Password</label>
-                        <input type="confirmPassword"/>
-                    </p>
-                    <p>
-                        <label className="textfield">User Handle</label>
-                        <input type="userHandle"/>
-                    </p>
-                </form>
-                <button>
-                <NavLink to="/">Sign Up!</NavLink>
-                </button>
-            </div>
+        return (
+            <div className="login" > 
+            <h3>Welcome to CoronaCraft!</h3>
+            <form style = {styles.form} noValidate onSubmit={this.handleSubmit}>
+        <div>
+          <textarea
+          id="email"
+          name="email"
+          type="email"
+          label="Email"
+          placeholder="email"
+          value={this.state.email}
+          onChange={this.handleChange}
+          fullWidth
+        />
+        </div>
+        
+        <div>
+        <textarea
+          id="password"
+          name="password"
+          type="password"
+          label="Password"
+          placeholder="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          fullWidth
+        />
+        </div>
+        <div>
+        <textarea
+          id="confirmPassword"
+          name="confirmPassword"
+          type="confirmPassword"
+          label="ConfirmPassword"
+          placeholder="confirm password"
+          value={this.state.confirmPassword}
+          onChange={this.handleChange}
+          fullWidth
+        />
+        </div>
+        <div>
+        <textarea
+          id="handle"
+          name="handle"
+          type="handle"
+          label="handle"
+          placeholder="UserName"
+          value={this.state.handle}
+          onChange={this.handleChange}
+          fullWidth
+        />
+        </div>
+        <button
+          type="submit"
+        >Signup
+        </button>
+        </form>
+        </div>
         );
     }
 }
@@ -41,4 +121,4 @@ const styles = {
     },
 }
 
-export default Signup;
+  export default Signup;
