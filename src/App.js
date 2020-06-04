f2ee3b86ecd24a2636e7c51fdf2a3c6c84ab85b7
Damini Kaushik <daminik@alphonse.lan>
2020-06-04 16:59:03 -0700

    added others tab

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import './App.css';
 import Home from './components/Home'
 
 import Projects from './components/Projects'
-import {BrowerRouter, Route, Link} from 'react-router-dom';
+import { BrowerRouter, Route, Link } from 'react-router-dom';
 import NavBar from './components/NavBar'
 import About from './components/About';
 import CreatePost from './components/CreatePost';
 import Baking from './components/Baking'
 import Masks from './components/Masks'
 import Gardening from './components/Gardening'
+import Other from './components/Other'
 
 import Login from './components/Login';
 import Signup from './components/Signup';
@@ -18,22 +19,23 @@ import Signup from './components/Signup';
 function App() {
   return (
     <div>
-    <NavBar />
+      <NavBar />
       <div className="App">
         <div className='content'>
-          <Route exact path="/" component={Home}/>
-          <Route path="/Login" component={Login}/>
-          <Route path="/Signup" component={Signup}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/Login" component={Login} />
+          <Route path="/Signup" component={Signup} />
           <div>
-            <Route exact path="/Projects" component={Projects}/>
+            <Route exact path="/Projects" component={Projects} />
             <div className='projects'>
-                  <Route path="/Projects/CreatePost" component={CreatePost}/>
-                  <Route exact path="/Projects/Baking" component={Baking}/>
-                  <Route exact path="/Projects/Masks" component={Masks}/>
-                  <Route exact path="/Projects/Gardening" component={Gardening}/>
+              <Route path="/Projects/CreatePost" component={CreatePost} />
+              <Route exact path="/Projects/Baking" component={Baking} />
+              <Route exact path="/Projects/Masks" component={Masks} />
+              <Route exact path="/Projects/Gardening" component={Gardening} />
+              <Route exact path="/Projects/Other" component={Other} />
             </div>
           </div>
-          <Route exact path="/About" component={About}/>
+          <Route exact path="/About" component={About} />
         </div>
       </div>
     </div>
